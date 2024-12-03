eecea312fd266d2165f5f2c7c8d1e47f0c87b16e
Jesse Pye <jessepye@gmail.com>
2024-12-03 11:19:27 -0600

    3a solved

---
+++ 3a/solver.py
@@ -0,0 +1,19 @@
+import re
+# import ipdb;
+
+file = open("input.txt", "r")
+
+text = file.read()
+pattern = r"mul\(\d{1,3},\d{1,3}\)"
+muls = re.findall(pattern, text)
+
+total = 0
+for mul in muls:
+    pattern = r"\d{1,3}"
+    nums = re.findall(pattern, mul)
+    nums = [int(n) for n in nums]
+    total += nums[0] * nums[1]
+
+print(total)
+
+
