c34515bc0437439b6abecb99d34190588f943248
Jesse Pye <jessepye@gmail.com>
2024-12-05 23:27:43 -0600

    day 5 complete

--- 5b/solver.py
+++ 5b/solver.py
@@ -24,21 +24,33 @@ with open(input_path, "r") as file:
                 nums = [int(n) for n in line.strip().split(",")]
                 updates.append(nums)
 
+# These functions rely on the list `rules` from the global scope
 
-total = 0
-
-
-for update in updates:
-    followsRules = True
+def isValid(update):
     for rule in rules:
         # ipdb.set_trace()
         if rule[0] in update and rule[1] in update and update.index(rule[0]) > update.index(rule[1]):
             print(f"update {update} breaks rule {rule}")
-            followsRules = False
-            break
-    if followsRules:
-        mid = update[int((len(update) - 1) / 2)]
+            return False
+    return True
+
+# bubble sort
+def fixUpdate(update):
+    for outer in range(1, len(update)):
+        for i in range(len(update) - outer):
+            if [update[i+1], update[i]] in rules:
+                update[i], update[i+1] = update[i+1], update[i]
+
+total = 0
+for update in updates:
+    if not isValid(update):
+        fixUpdate(update)
+        print(f"Fix attempted. valid? {isValid(update)}")
+        print(f"fixed: {update}")
+        mid = update[int((len(update) - 1)/2)]
         total += mid
-        print(f"update {update} follows all rules; mid {mid}; total {total}")
+        print(f"mid {mid} new total {total}")
+        
 
 print(total)
+
