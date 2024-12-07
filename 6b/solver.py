d08a6c2b955b2e26774ea8c40a7743cfc3869721
Jesse Pye <jessepye@gmail.com>
2024-12-07 13:55:17 -0600

    day 6b complete

--- 6b/solver.py
+++ 6b/solver.py
@@ -1,55 +1,90 @@
-import ipdb;
+import ipdb
 import os
+import copy
 
 # Get the directory containing the current script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 # Construct the path to input.txt relative to the script location
 input_path = os.path.join(script_dir, "input.txt")
+solved_room_path = os.path.join(script_dir, "solved_room.txt")
 
-room = []
-
-def printRoom():
+def printRoom(room):
     for line in room:
         for c in line:
             print(c, end="")
         print()
 
-with open(input_path, "r") as file:
-    room = [list(line) for line in file.read().strip().split("\n")]
+originalRoom = []
 
-dirs = ((0, -1), (1, 0), (0, 1), (-1, 0))  # dx, dy
-currentDir = 0 # (0, -1), which represents North
+with open(input_path, "r") as file:
+    originalRoom = [list(line) for line in file.read().strip().split("\n")]
+
+solvedRoom = []
+
+with open(solved_room_path, "r") as file:
+    solvedRoom = [list(line) for line in file.read().strip().split("\n")]
+
 
 # find the starting position
 x, y = 0, 0
-for y in range(len(room)):
-    if '^' in room[y]:
-        x = room[y].index('^')
+for y in range(len(originalRoom)):
+    if '^' in originalRoom[y]:
+        x = originalRoom[y].index('^')
         break
 
-print(x, y)
+startingX , startingY = x, y
+print(f"starting position ({x}, {y})")
 
-while True:
-    room[y][x] = 'X'
-    # printRoom()
-    nextX = x + dirs[currentDir][0]
-    nextY = y + dirs[currentDir][1]
-    # Are we still in the room?
-    # ipdb.set_trace()
-    if nextX < 0 or nextX >= len(room[y]) or nextY < 0 or nextY >= len(room):
-        print("we have left the room")
-        break
-    nextLoc = room[nextY][nextX]
-    if nextLoc == '#':
-        currentDir = (currentDir + 1) % (len(dirs))
-        continue
-    if nextLoc == '.' or nextLoc == 'X':
-        x = nextX
-        y = nextY
-        continue
-    print("we shouldn't be here...")
+def producesLoop(room, x, y) -> bool:
+    dirs = ((0, -1), (1, 0), (0, 1), (-1, 0))  # dx, dy
+    currentDir = 0 # (0, -1), which represents North
+    numSteps = 0
+    # TODO: more effecient loop detection
+    maxPossibleSteps = len(room) * len(room[0]) * 4
+    while True:
+        # room[y][x] = 'X'
+        numSteps += 1
 
-total = sum(line.count('X') for line in room)
-printRoom()
+        if numSteps > maxPossibleSteps:
+            return True
+
+        nextX = x + dirs[currentDir][0]
+        nextY = y + dirs[currentDir][1]
+
+        # Are we still in the room?
+        if nextX < 0 or nextX >= len(room[y]) or nextY < 0 or nextY >= len(room):
+            print("we have left the room")
+            return False
+
+        nextLoc = room[nextY][nextX]
+
+        # found wall, rotate
+        if nextLoc == '#': 
+            currentDir = (currentDir + 1) % (len(dirs))
+            continue
+        else:
+            x = nextX
+            y = nextY
+            continue
+
+print(f"Original Room:")
+printRoom(originalRoom)
+
+print(f"Solved Room")
+printRoom(solvedRoom)
+
+# Change each 'X' one at a time to a '#' and see if it produces a loop
+
+total = 0
+
+for y in range(len(solvedRoom)):
+    for x in range(len(solvedRoom[y])):
+        if solvedRoom[y][x] == 'X':
+            modifiedRoom = copy.deepcopy(originalRoom)
+            modifiedRoom[y][x] = '#'
+            if producesLoop(modifiedRoom, startingX, startingY):
+                total += 1
+                print(f"Found a loop when placing a wall at {x}, {y}. New total: {total}")
+
 print(total)
 
