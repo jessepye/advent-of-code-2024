67f3f0cbbd3c844b347e3ff3c2ac55ce1c708f57
Jesse Pye <jessepye@gmail.com>
2024-12-09 23:59:36 -0600

    9b complete

--- 9b/solver.py
+++ 9b/solver.py
@@ -1,5 +1,7 @@
-# inputFile = "input.txt"
-inputFile = "example.txt"
+import ipdb
+
+inputFile = "input.txt"
+# inputFile = "example.txt"
 with open(inputFile, "r") as file:
     diskMap = file.read().strip()
 
@@ -32,16 +34,40 @@ def findNextFreeBlockIndex(blocks, current) -> int:
         if blocks[i] == None:
             return i
 
-def findInBlocks(blocks, i):
-    start = 0
+def findInBlocks(blocks, target):
+    start = None
+    try:
+        start = blocks.index(target)
+    except ValueError:
+        return None
+    for i in range(start, len(blocks)):
+        if blocks[i] != target:
+            return start, i
+    return start, len(blocks)
+
+def findOpening(blocks, size):
+    currentSize = 0
     for i in range(len(blocks)):
-        pass # TODO: continue here
+        if blocks[i] == None:
+            currentSize += 1
+            if currentSize >= size:
+                return i - currentSize + 1
+        else:
+            currentSize = 0
+    return None
 
 # Start at the last id, iterate backwards
 for i in range(lastId, -1, -1):
     print(f"i: {i}")
     start, end = findInBlocks(blocks, i)
-
+    size = end - start
+    openingIndex = findOpening(blocks, size)
+    # ipdb.set_trace()
+    if openingIndex != None and openingIndex < start:
+        dataIndex = start
+        for j in range(openingIndex, openingIndex + size):
+            blocks[j], blocks[dataIndex] = blocks[dataIndex], blocks[j]
+            dataIndex += 1
 
 # calculate checksum:
 checkSum = 0
