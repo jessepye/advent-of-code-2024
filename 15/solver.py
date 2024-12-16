85b796a290f6679b0cd7000b0525ac2bbaef1409
Jesse Pye <jessepye@gmail.com>
2024-12-15 20:16:41 -0600

    create day 15

---
+++ 15/solver.py
@@ -0,0 +1,20 @@
+# import ipdb; ipdb.set_trace()
+
+def solve_part_one(inputFile):
+    with open(inputFile, "r") as file:
+        inputText = file.read()
+    print(inputText)
+
+def solve_part_two():
+    pass
+
+def main():
+    # inputFile = "input.txt"
+    inputFile = "example.txt"
+
+    solve_part_one(inputFile)
+
+    solve_part_two()
+
+if __name__ == "__main__":
+    main()
