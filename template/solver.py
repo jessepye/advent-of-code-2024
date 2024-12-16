de29053ef1860c83af53b21a6e158dc9d4c8ed42
Jesse Pye <jessepye@gmail.com>
2024-12-15 20:02:02 -0600

    update template

--- template/solver.py
+++ template/solver.py
@@ -1,7 +1,20 @@
 # import ipdb; ipdb.set_trace()
-# inputFile = "input.txt"
-inputFile = "example.txt"
 
-with open(inputFile, "r") as file:
+def solve_part_one():
     pass
 
+def solve_part_two():
+    pass
+
+def main():
+    # inputFile = "input.txt"
+    inputFile = "example.txt"
+    with open(inputFile, "r") as file:
+        pass
+
+    solve_part_one()
+
+    solve_part_two()
+
+if __name__ == "__main__":
+    main()
